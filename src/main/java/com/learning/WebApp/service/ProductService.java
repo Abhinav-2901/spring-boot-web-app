e3bf004307eddbdd55ecb9b0927a47ddb70e017d
Abhinav Chauhan <abhinavcj29@gmail.com>
2025-01-12 17:32:06 +0530

    JPA and H2 setup

--- src/main/java/com/learning/WebApp/service/ProductService.java
+++ src/main/java/com/learning/WebApp/service/ProductService.java
@@ -9,7 +9,11 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    public List<Product> products =new ArrayList<>(Arrays.asList(new Product(101,"Iphone",70000), new Product(102,"Camera", 60000), new Product(103, "MacBook Air M2", 90000)));
+    public List<Product> products =new ArrayList<>(Arrays.asList(
+            new Product(101,"Iphone",70000),
+            new Product(102,"Camera", 60000),
+            new Product(103, "MacBook Air M2", 90000)
+    ));
 
     public List<Product> getProducts(){
         return products;
